ea7b664c8ad610797aed40dd5822c3d85634a27b
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2023-09-15 12:58:29 -0300

    login

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
+import { Text, Button, Input, Icon } from 'react-native-elements';
+import styles from './styles/Styles';
 
-export default function App() {
+  export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text h3 style = {styles.texto}>Login</Text>
       <StatusBar style="auto" />
+
+   <Input style = {styles.Input}  
+        placeholder='E-mail'
+        leftIcon={{ type: 'font-awesome', name: 'envelope' , color:'white' }}
+         keyboardType='email-address'
+   
+   />
+
+   <Input style = {styles.Input} 
+          placeholder='Senha'
+          leftIcon ={{type: 'font-awesome', name:'lock', color: 'white'}}
+          secureTextEntry={true}
+
+   />
+
+<Button 
+              title="Entrar"
+              icon={{
+                name: 'check',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles/Styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#196774',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    Input:
+    {
+        color:'white', 
+        fontSize:24,   
+        
+    },
+    texto:
+    {
+        color:'white',
+        bottom:40,
+    },
+    
+    
+
+    
+    
+        
+    
+  });
+export default styles;
